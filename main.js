c13724734af8368b2af2392e2055eeb0dd1882a3
tatakuku <sakaitakumi0622@gmail.com>
2026-01-16 00:37:52 +0900

    updated

--- main.js
+++ main.js
@@ -1,3 +1,40 @@
 "use strict";
+var dayToEnglish = function (dayNum) {
+    var english = '';
+    switch (dayNum) {
+        case 0:
+            english = 'sunday';
+            break;
+        case 1:
+            english = 'monday';
+            break;
+        case 2:
+            english = 'tuesday';
+            break;
+        case 3:
+            english = 'wednesday';
+            break;
+        case 4:
+            english = 'thursday';
+            break;
+        case 5:
+            english = 'friday';
+            break;
+        case 6:
+            english = 'saturday';
+            break;
+    }
+    return { ordinal: dayNum, english: english };
+};
 var now = new Date();
-console.log(now);
+console.log(now.toLocaleString());
+console.log(now.getFullYear());
+console.log(now.getMonth() + 1);
+console.log(now.getDate());
+console.log(now.getHours());
+console.log(now.getMinutes());
+console.log(now.getSeconds());
+console.log(now.getMilliseconds());
+console.log(now.getDay());
+var dayOfToday = dayToEnglish(now.getDay());
+console.log(dayOfToday.english.toUpperCase());
--- main.ts
+++ main.ts
@@ -1,2 +1,28 @@
+type Day = { ordinal: number, english: string };
+const dayToEnglish = (dayNum: number): Day => {
+    let english: string = '';
+    switch (dayNum) {
+        case 0: english = 'sunday'; break;   
+        case 1: english = 'monday'; break;   
+        case 2: english = 'tuesday'; break;   
+        case 3: english = 'wednesday'; break;   
+        case 4: english = 'thursday'; break;   
+        case 5: english = 'friday'; break;   
+        case 6: english = 'saturday'; break;   
+    }
+    return {ordinal: dayNum, english: english};
+}
+
 const now: Date = new Date();
-console.log(now);
+console.log(now.toLocaleString());
+console.log(now.getFullYear());
+console.log(now.getMonth() + 1);
+console.log(now.getDate());
+console.log(now.getHours());
+console.log(now.getMinutes());
+console.log(now.getSeconds());
+console.log(now.getMilliseconds());
+console.log(now.getDay());
+
+const dayOfToday: Day = dayToEnglish(now.getDay());
+console.log(dayOfToday.english.toUpperCase());
